04b639a759f9d48e17d1b1ba38f75eb5c7f629d7
Busiwa24 <busiwaliuma@gmail.com>
2022-01-04 15:42:16 +0200

    Add author and label methods

--- classes/item.rb
+++ classes/item.rb
@@ -15,7 +15,17 @@ class Item
       @genre = genre
       genre.items.push(self) unless genre.items.include?(self)
     end
-  
+
+    def add_label(label)
+        @label = label
+        label.items. push(self) unless label.items. include? (self)
+    end
+
+    def add_author(author)
+        @author = author
+        author.items. push(self) unless author.items. include? (self)
+    end
+
     private
   
     def can_be_archived?
