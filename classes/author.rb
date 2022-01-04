0452e6165ac77f0462c54afb889cedcf12afb745
brianorora <brianorora@gmail.com>
2022-01-04 17:48:29 +0300

    Fix: initialize `Author` class properties
    - `@first_name`
    - `@last_name`

--- classes/author.rb
+++ classes/author.rb
@@ -1,16 +1,16 @@
 require 'math'
 
 class Author
-
-  attr_reader: first_name, last_name
+  attr_reader :first_name, :last_name
 
   def initialize(first_name, last_name)
     @id = rand(1..100_000)
     @items = []
+    @first_name = first_name
+    @last_name = last_name
   end
 
   def add_item(item)
     items.push(item) unless @items.include? item
   end
-
 end
