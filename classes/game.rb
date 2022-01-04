32b711f666533b9c33f453c2dc8ed610b188eaab
brianorora <brianorora@gmail.com>
2022-01-04 17:49:17 +0300

    Initialize `Game` class properties
    - `@last_played_date`
    - `@multiplayer`

--- classes/game.rb
+++ classes/game.rb
@@ -1,3 +1,9 @@
+require_relative './item'
 
-class Game
+class Game < Item
+  def initialize(publish_date, multiplayer, last_played_date)
+    super(publish_date)
+    @last_played_date = last_played_date
+    @multiplayer = multiplayer
+  end
 end
