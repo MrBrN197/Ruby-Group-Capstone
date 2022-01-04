568a2e80cabbd4db1298006322c08e5a9c776ea0
brianorora <brianorora@gmail.com>
2022-01-04 17:32:24 +0300

    Initialize `Author` class properties

--- classes/author.rb
+++ classes/author.rb
@@ -1,10 +1,16 @@
+require 'math'
 
 class Author
 
-  attr_accessor :items
+  attr_reader: first_name, last_name
 
-  def initialize
+  def initialize(first_name, last_name)
+    @id = rand(1..100_000)
     @items = []
+  end
+
+  def add_item(item)
+    items.push(item) unless @items.include? item
   end
 
 end
