8a2987de5c274d08b53973893a2b94113e7d07d1
leolpaz <leonardolpaz95@gmail.com>
2022-01-05 08:55:44 -0300

    Create bookfunctions module with save and load and add book functions

---
+++ modules/bookfunctions.rb
@@ -0,0 +1,78 @@
+require_relative '../classes/book'
+require 'json'
+
+module BookFunctions
+  def list_all_books
+    @book_list.each_with_index do |book, i|
+      puts "#{i+1}) publisher: #{book.publisher}, cover state: #{book.cover_state}, publish date: #{book.publish_date.year}"
+    end
+  end
+
+  def list_all_labels
+    @label_list.each_with_index do |label, i|
+      puts "#{i+1}) title: #{label.title}, color: #{label.color}"
+    end
+  end
+
+  def get_text(text)
+    valid = false
+    until valid
+      print "Insert #{text}:"
+      input = gets.chomp
+      valid = true unless input == ""
+    end
+    input
+  end
+
+  def get_cover_state
+    cover_state = ""
+    valid = false
+    while cover_state == ""
+      print "Insert cover state[good/bad]:"
+      cover_state = gets.chomp
+      valid = cover_state == 'good' || cover_state == 'bad'
+      cover_state = "" unless valid
+      puts "Please insert good or bad as the cover state" unless valid
+    end
+    cover_state
+  end
+
+  def get_publish_date
+    publish_date = '0'
+    until !!(publish_date =~ /\d{4,4}/) && publish_date.to_i.between?(1, 2022) 
+      print "Insert publish_date[year, 4 digits]:"
+      publish_date = gets.chomp
+    end
+    publish_date
+  end
+
+  def create_book(publisher, cover_state, publish_date)
+    book = Book.new(publisher, cover_state, publish_date)
+    @book_list.push(book)
+  end
+
+  def add_book
+    publisher = get_text('publisher')
+    cover_state = get_cover_state()
+    publish_date = get_publish_date().to_i
+    create_book(publisher, cover_state, publish_date)
+  end
+
+  def save_books
+    book_list = @book_list.each_with_index.map do |book, index|
+      { 'publisher' => book.publisher, 'cover_state' => book.cover_state, 'publish_date' => book.publish_date}
+    end
+    File.write('book_list.json', JSON.generate(book_list))
+  end
+
+  def load_books
+    return [] unless File.exist?('book_list.json')
+    raw_book_list = File.read('book_list.json')
+    raw_book_list = JSON.parse(raw_book_list)
+    arr = []
+    raw_book_list.each do |book|
+      arr.push(Book.new(book['publisher'], book['cover_state'], book['publish_date'].to_i))
+    end
+    arr
+  end
+end
