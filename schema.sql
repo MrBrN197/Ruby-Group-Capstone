05867138793e7ce46a8586f4e1ba7c376e667581
brianorora <brianorora@gmail.com>
2022-01-07 00:22:16 +0300

    Add music_albums and genres schema tables

--- schema.sql
+++ schema.sql
@@ -12,6 +12,26 @@ CREATE TABLE authors(
   PRIMARY KEY(id)
 )
 
+CREATE TABLE genres(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name CHAR(50),
+  PRIMARY KEY(id)
+)
+
+CREATE TABLE music_albums(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  on_spotify BOOLEAN,
+  published_date DATE,
+  archived BOOLEAN,
+  genre_id INT,
+  author_id INT,
+  label_id  INT,
+  FOREIGN KEY(genre_id) REFERENCES genres(id),
+  FOREIGN KEY(author_id) REFERENCES authors(id),
+  FOREIGN KEY(label_id) REFERENCES labels(id),
+  PRIMARY KEY(id)
+)
+
 CREATE TABLE books(
   id INT GENERATED ALWAYS AS IDENTITY,
   publisher CHAR(50),
@@ -24,7 +44,7 @@ CREATE TABLE books(
   FOREIGN KEY(genre_id) REFERENCES genres(id),
   FOREIGN KEY(author_id) REFERENCES authors(id),
   FOREIGN KEY(label_id) REFERENCES labels(id),
-  PRIMARY KEY(author_id)
+  PRIMARY KEY(id)
 )
 
 CREATE TABLE games(
