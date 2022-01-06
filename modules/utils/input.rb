1c7185c097c9d7e5c6213fd6120710b34949891c
brianorora <brianorora@gmail.com>
2022-01-06 14:25:37 +0300

    Add `ask_year` to `Input` module

--- modules/utils/input.rb
+++ modules/utils/input.rb
@@ -8,4 +8,13 @@ module Input
     end
     input
   end
+
+  def ask_year(text)
+    year = '0'
+    until !(year =~ /\d{4,4}/).nil? && year.to_i.between?(1, 2022)
+      print "Insert #{text} [year, 4 digits]:"
+      year = gets.chomp
+    end
+    year
+  end
 end
