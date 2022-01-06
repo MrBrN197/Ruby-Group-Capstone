ebe145c12ec88756afe0a5aa4da7329ceede7d8d
brianorora <brianorora@gmail.com>
2022-01-07 00:04:24 +0300

    Fix: `Input` text spacing

--- modules/utils/input.rb
+++ modules/utils/input.rb
@@ -2,7 +2,7 @@ module Input
   def ask_text(text)
     valid = false
     until valid
-      print "Insert #{text}:"
+      print "Insert #{text}: "
       input = gets.chomp
       valid = true unless input == ''
     end
@@ -12,7 +12,7 @@ module Input
   def ask_year(text)
     year = '0'
     until !(year =~ /\d{4,4}/).nil? && year.to_i.between?(1, 2022)
-      print "Insert #{text} [year, 4 digits]:"
+      print "Insert #{text} [year, 4 digits]: "
       year = gets.chomp
     end
     year
